3efad72606aced803d5ab6d747980d2c61f6ef74
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-10-30 18:49:21 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 Assignments/2.1 CC_Percolation/CC.java
+++ ADS-2 Assignments/2.1 CC_Percolation/CC.java
@@ -1,15 +1,29 @@
+/**
+ * Class for cc.
+ */
 public class CC {
-    private boolean[] marked;   // marked[v] = has vertex v been marked?
-    private int[] id;           // id[v] = id of connected component containing v
-    private int[] size;         // size[id] = number of vertices in given component
-    private int count;          // number of connected components
+	/**
+	 * marked[v] = has vertex v been marked?.
+	 */
+    private boolean[] marked;
+    /**
+     * id[v] = id of connected component containing v.
+     */
+    private int[] id;
+    /**
+     * size[id] = number of vertices in given component.
+     */
+    private int[] size;
+    /**
+     * number of connected components.
+     */
+    private int count;
 
     /**
      * Computes the connected components of the undirected graph {@code G}.
-     *
      * @param G the undirected graph
      */
-    public CC(Graph G) {
+    public CC(final Graph G) {
         marked = new boolean[G.ve()];
         id = new int[G.ve()];
         size = new int[G.ve()];
@@ -20,9 +34,12 @@ public class CC {
             }
         }
     }
-
-    // depth-first search for a Graph
-    private void dfs(Graph G, int v) {
+    /**
+     * depth-first search for a Graph.
+     * @param      G     G of type Graph.
+     * @param      v     v of type int.
+     */
+    private void dfs(final Graph G, final int v) {
         marked[v] = true;
         id[v] = count;
         size[count]++;
@@ -35,57 +52,21 @@ public class CC {
 
     /**
      * Returns the component id of the connected component containing vertex {@code v}.
-     *
      * @param  v the vertex
      * @return the component id of the connected component containing vertex {@code v}
-     * @throws IllegalArgumentException unless {@code 0 <= v < V}
      */
     public int id(int v) {
-        validateVertex(v);
         return id[v];
     }
-
-    /**
-     * Returns the number of vertices in the connected component containing vertex {@code v}.
-     *
-     * @param  v the vertex
-     * @return the number of vertices in the connected component containing vertex {@code v}
-     * @throws IllegalArgumentException unless {@code 0 <= v < V}
-     */
-    public int size(int v) {
-        validateVertex(v);
-        return size[id[v]];
-    }
-
-    /**
-     * Returns the number of connected components in the graph {@code G}.
-     *
-     * @return the number of connected components in the graph {@code G}
-     */
-    public int count() {
-        return count;
-    }
-
     /**
      * Returns true if vertices {@code v} and {@code w} are in the same
      * connected component.
-     *
      * @param  v one vertex
      * @param  w the other vertex
      * @return {@code true} if vertices {@code v} and {@code w} are in the same
      *         connected component; {@code false} otherwise
-     * @throws IllegalArgumentException unless {@code 0 <= v < V}
-     * @throws IllegalArgumentException unless {@code 0 <= w < V}
      */
-    public boolean connected(int v, int w) {
-        validateVertex(v);
-        validateVertex(w);
+    public boolean connected(final int v, final int w) {
         return id(v) == id(w);
-    }
-    // throw an IllegalArgumentException unless {@code 0 <= v < V}
-    private void validateVertex(int v) {
-        int V = marked.length;
-        if (v < 0 || v >= V)
-            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
     }
 }
