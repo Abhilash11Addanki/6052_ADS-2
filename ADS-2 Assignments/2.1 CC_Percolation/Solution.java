bf6397e9657224673dba60b38d6b181c02445eff
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-10-30 18:58:59 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 Assignments/2.1 CC_Percolation/Solution.java
+++ ADS-2 Assignments/2.1 CC_Percolation/Solution.java
@@ -1,12 +1,24 @@
 import java.util.Scanner;
-class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int vertices = sc.nextInt();
-		Percolation p = new Percolation(vertices);
-		while (sc.hasNext()) {
-			p.open(sc.nextInt() - 1, sc.nextInt() - 1);
-		}
-		System.out.println(p.percolates());
-	}
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**
+     * main method that drives the program.
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int vertices = sc.nextInt();
+        Percolation p = new Percolation(vertices);
+        while (sc.hasNext()) {
+            p.open(sc.nextInt() - 1, sc.nextInt() - 1);
+        }
+        System.out.println(p.percolates());
+    }
 }
