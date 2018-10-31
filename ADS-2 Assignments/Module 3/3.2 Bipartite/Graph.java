9726ef387b84bb58b8893138c329c1fb027ff73f
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-10-31 19:27:33 +0530

    "ads2_m3_a2_1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 Assignments/Module 3/3.2 Bipartite/Graph.java
+++ ADS-2 Assignments/Module 3/3.2 Bipartite/Graph.java
@@ -1,155 +1,110 @@
-/*************************************************************************
- *  Compilation:  javac Graph.java        
- *  Execution:    java Graph input.txt
- *  Dependencies: Bag.java In.java StdOut.java
- *  Data files:   http://algs4.cs.princeton.edu/41undirected/tinyG.txt
- *
- *  A graph, implemented using an array of sets.
- *  Parallel edges and self-loops allowed.
- *
- *  % java Graph tinyG.txt
- *  13 vertices, 13 edges 
- *  0: 6 2 1 5 
- *  1: 0 
- *  2: 0 
- *  3: 5 4 
- *  4: 5 6 3 
- *  5: 3 4 0 
- *  6: 0 4 
- *  7: 8 
- *  8: 7 
- *  9: 11 10 12 
- *  10: 9 
- *  11: 9 12 
- *  12: 11 9 
- *
- *  % java Graph mediumG.txt
- *  250 vertices, 1273 edges 
- *  0: 225 222 211 209 204 202 191 176 163 160 149 114 97 80 68 59 58 49 44 24 15 
- *  1: 220 203 200 194 189 164 150 130 107 72 
- *  2: 141 110 108 86 79 51 42 18 14 
- *  ...
- *  
- *************************************************************************/
-
-
 /**
- *  The <tt>Graph</tt> class represents an undirected graph of vertices
- *  named 0 through V-1.
- *  It supports the following operations: add an edge to the graph,
- *  iterate over all of the neighbors adjacent to a vertex.
- *  Parallel edges and self-loops are permitted.
- *  <p>
- *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/51undirected">Section 5.1</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * Class for graph.
  */
-public class Graph {
-    private final int V;
-    private int E;
+class Graph {
+    /**
+     * integer variable vertices.
+     */
+    private int vertices;
+    /**
+     * integer variable edges.
+     */
+    private int edges;
+    /**
+     * array of bag type.
+     */
     private Bag<Integer>[] adj;
-    
-   /**
-     * Create an empty graph with V vertices.
+    /**
+     * Constructs the object.
      */
-    public Graph(int V) {
-        if (V < 0) throw new RuntimeException("Number of vertices must be nonnegative");
-        this.V = V;
-        this.E = 0;
-        adj = (Bag<Integer>[]) new Bag[V];
-        for (int v = 0; v < V; v++) {
-            adj[v] = new Bag<Integer>();
+    Graph() {
+
+    }
+    /**
+     * Constructs the object.
+     * @param      vertex     integer variable.
+     */
+    Graph(final int vertex) {
+        vertices = vertex;
+        edges = 0;
+        adj = (Bag<Integer>[]) new Bag[vertex];
+        for (int i = 0; i < vertex; i++) {
+            adj[i] = new Bag<Integer>();
         }
     }
-
-   /**
-     * Create a random graph with V vertices and E edges.
-     * Expected running time is proportional to V + E.
+    /**
+     * returns vertices.
+     * Time complexity is O(1).
+     * @return  vertices.
      */
-    public Graph(int V, int E) {
-        this(V);
-        if (E < 0) throw new RuntimeException("Number of edges must be nonnegative");
-        for (int i = 0; i < E; i++) {
-            int v = (int) (Math.random() * V);
-            int w = (int) (Math.random() * V);
-            addEdge(v, w);
+    public int ve() {
+        return vertices;
+    }
+    /**
+     * returns edges.
+     * Time complexity is O(1).
+     * @return edges.
+     */
+    public int e() {
+        return edges;
+    }
+    /**
+     * Adds an edge.
+     * Time complexity is O(1)
+     * @param      v     integer variable.
+     * @param      w     integer variable.
+     */
+    public void addEdge(final int v, final int w) {
+        if (!hasEdge(v, w)) {
+            edges++;
+            adj[v].add(w);
+            adj[w].add(v);
+        }
+        if (v == w) {
+            return;
         }
     }
-
-   /**  
-     * Create a digraph from input stream.
+    /**
+     * Determines if it has edge.
+     * @param      v     integer variable.
+     * @param      w     integer variable.
+     * Time complexity is O(v)
+     * @return     True if has edge, False otherwise.
      */
-/*    public Graph(In in) {
-        this(in.readInt());
-        int E = in.readInt();
-        for (int i = 0; i < E; i++) {
-            int v = in.readInt();
-            int w = in.readInt();
-            addEdge(v, w);
+    public boolean hasEdge(final int v, final int w) {
+        if (adj[v] == null) {
+            return true;
         }
-    }*/
-
-   /**
-     * Copy constructor.
-     */
-/*    public Graph(Graph G) {
-        this(G.V());
-        this.E = G.E();
-        for (int v = 0; v < G.V(); v++) {
-            // reverse so that adjacency list is in same order as original
-            Stack<Integer> reverse = new Stack<Integer>();
-            for (int w : G.adj[v]) {
-                reverse.push(w);
+        for (int i : adj[v]) {
+            if (i == w) {
+                return true;
             }
-            for (int w : reverse) {
-                adj[v].add(w);
-            }
         }
-    }*/
-
-   /**
-     * Return the number of vertices in the graph.
-     */
-    public int V() { return V; }
-
-   /**
-     * Return the number of edges in the graph.
-     */
-    public int E() { return E; }
-
-
-   /**
-     * Add the edge v-w to graph.
-     */
-    public void addEdge(int v, int w) {
-        E++;
-        adj[v].add(w);
-        adj[w].add(v);
+        return false;
     }
-
-
-   /**
-     * Return the list of neighbors of vertex v as in Iterable.
+    /**
+     * iterable function.
+     * @param      v integer variable.
+     * Time complexity is O(v)
+     * @return   array.
      */
-    public Iterable<Integer> adj(int v) {
+    public Iterable<Integer> adj(final int v) {
         return adj[v];
     }
-
-
-   /**
-     * Return a string representation of the graph.
+    /**
+     * matrix method.
+     * Time complexity is O(1)
+     * @return   array.
      */
-    public String toString() {
-        StringBuilder s = new StringBuilder();
-        String NEWLINE = System.getProperty("line.separator");
-        s.append(V + " vertices, " + E + " edges " + NEWLINE);
-        for (int v = 0; v < V; v++) {
-            s.append(v + ": ");
-            for (int w : adj[v]) {
-                s.append(w + " ");
-            }
-            s.append(NEWLINE);
-        }
-        return s.toString();
+    public Bag[] matrix() {
+        return adj;
     }
-
+    /**
+     * list method.
+     * Time complexity is O(1)
+     * @return  array.
+     */
+    public Bag[] list() {
+        return adj;
+    }
 }
