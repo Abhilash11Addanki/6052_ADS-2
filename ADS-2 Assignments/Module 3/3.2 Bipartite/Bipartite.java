ce7f48412ad2be82a229574595be7b448df7a5e4
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-10-31 19:19:33 +0530

    "ads2_m3_a2_1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2 Assignments/Module 3/3.2 Bipartite/Bipartite.java
@@ -0,0 +1,91 @@
+/**
+ * Class for bipartite.
+ */
+public class Bipartite {
+    /**
+     * is the graph bipartite?.
+     */
+    private boolean isBipartite;
+    private boolean[] color;       // color[v] gives vertices on one side of bipartition
+    private boolean[] marked;      // marked[v] = true iff v has been visited in DFS
+    private int[] edgeTo;          // edgeTo[v] = last edge on path to v
+    private Stack<Integer> cycle;  // odd-length cycle
+
+    /**
+     * Determines whether an undirected graph is bipartite and finds either a
+     * bipartition or an odd-length cycle.
+     *
+     * @param  G the graph
+     */
+    public Bipartite(Graph G) {
+        isBipartite = true;
+        color  = new boolean[G.V()];
+        marked = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+
+        for (int v = 0; v < G.V(); v++) {
+            if (!marked[v]) {
+                dfs(G, v);
+            }
+        }
+    }
+
+    private void dfs(Graph G, int v) { 
+        marked[v] = true;
+        for (int w : G.adj(v)) {
+
+            // short circuit if odd-length cycle found
+            if (cycle != null) return;
+
+            // found uncolored vertex, so recur
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                color[w] = !color[v];
+                dfs(G, w);
+            } 
+
+            // if v-w create an odd-length cycle, find it
+            else if (color[w] == color[v]) {
+                isBipartite = false;
+                cycle = new Stack<Integer>();
+                cycle.push(w);  // don't need this unless you want to include start vertex twice
+                for (int x = v; x != w; x = edgeTo[x]) {
+                    cycle.push(x);
+                }
+                cycle.push(w);
+            }
+        }
+    }
+
+    /**
+     * Returns true if the graph is bipartite.
+     *
+     * @return {@code true} if the graph is bipartite; {@code false} otherwise
+     */
+    public boolean isBipartite() {
+        return isBipartite;
+    }
+ 
+    /**
+     * Returns the side of the bipartite that vertex {@code v} is on.
+     *
+     * @param  v the vertex
+     * @return the side of the bipartition that vertex {@code v} is on; two vertices
+     *         are in the same side of the bipartition if and only if they have the
+     *         same color
+     */
+    public boolean color(int v) {
+        return color[v];
+    }
+    /**
+     * Returns an odd-length cycle if the graph is not bipartite, and
+     * {@code null} otherwise.
+     *
+     * @return an odd-length cycle if the graph is not bipartite
+     *         (and hence has an odd-length cycle), and {@code null}
+     *         otherwise
+     */
+    public Iterable<Integer> oddCycle() {
+        return cycle; 
+    }
+}
---
+++ ADS-2 Assignments/Module 3/3.2 Bipartite/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int vertices = Integer.parseInt(sc.nextLine());
+		int edges = Integer.parseInt(sc.nextLine());
+		Graph g = new Graph(vertices);
+		for (int i = 0; i < edges; i++) {
+			String[] inp = sc.nextLine().split(" ");
+			g.addEdge(Integer.parseInt(inp[0]), Integer.parseInt(inp[1]));
+		}
+		Bipartite b = new Bipartite(g);
+		if (b.isBipartite()) {
+			System.out.println("Graph is bipartite");
+		} else {
+			System.out.println("Graph is not a bipartite");
+		}
+	}
+}
