892b9488c52b53944aac3dc23763df38a622d177
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-11-01 18:49:04 +0530

    "ads2_m4_a1_1: 0/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2 Assignments/Module 4/Code Camp - WordNet/WordNet/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.io.*;
+class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String synsets = sc.nextLine();
+		File file = new File("Files/" + synsets);
+		try {
+			Scanner scan = new Scanner(file);
+			while (scan.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
