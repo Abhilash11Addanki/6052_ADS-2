71873c5b50e76590e82013a3c1f0c32c21b1e287
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-11-01 19:48:43 +0530

    "ads2_m4_a1_1: 0/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 Assignments/Module 4/Code Camp - WordNet/WordNet/Solution.java
+++ ADS-2 Assignments/Module 4/Code Camp - WordNet/WordNet/Solution.java
@@ -1,17 +1,23 @@
-import java.util.Scanner;
+import java.util.*;
 import java.io.*;
 class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String synsets = sc.nextLine();
+		String hypernyms = sc.nextLine();
 		File file = new File("Files\\" + synsets);
+		HashMap<Integer, String> synsetshm = new HashMap<Integer, String>();
 		try {
 			Scanner scan = new Scanner(file);
 			while (scan.hasNextLine()) {
-				System.out.println(sc.nextLine());
+				String[] inp = scan.nextLine().split(",");
+				synsetshm.put(Integer.parseInt(inp[0]), inp[1]);
 			}
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
+		}
+		for (Map.Entry m : synsetshm.entrySet()) {
+			System.out.println(m.getKey() + " " + m.getValue());
 		}
 	}
 }
