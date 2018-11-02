7b10fbfaf5f6158920623fbbbdee945189965b96
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-11-02 14:06:53 +0530

    "ads2_m4_a1_1: 1/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 Assignments/Module 4/Code Camp - WordNet/WordNet/Solution.java
+++ ADS-2 Assignments/Module 4/Code Camp - WordNet/WordNet/Solution.java
@@ -4,20 +4,44 @@ class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String synsets = sc.nextLine();
-		String hypernyms = sc.nextLine();
 		File file = new File("Files\\" + synsets);
-		HashMap<Integer, String> synsetshm = new HashMap<Integer, String>();
+		HashMap<Integer, ArrayList<String>> synsetshm = new HashMap<Integer, ArrayList<String>>();
+		HashMap<String, ArrayList<Integer>> revsynsetshm = new HashMap<String, ArrayList<Integer>>();
 		try {
 			Scanner scan = new Scanner(file);
 			while (scan.hasNextLine()) {
+				ArrayList<String> al = new ArrayList<String>();
+				ArrayList<Integer> al2 = new ArrayList<Integer>();
 				String[] inp = scan.nextLine().split(",");
-				synsetshm.put(Integer.parseInt(inp[0]), inp[1]);
+				String[] tokens = inp[1].split(" ");
+				String[] tokens1 = inp[0].split(" ");
+				for (String s : tokens) {
+					al.add(s);
+				}
+				for (String i : tokens1) {
+					al2.add(Integer.parseInt(i));
+				}
+				synsetshm.put(Integer.parseInt(inp[0]), al);
+				revsynsetshm.put(inp[1], al2);
 			}
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		for (Map.Entry m : synsetshm.entrySet()) {
-			System.out.println(m.getKey() + " " + m.getValue());
+		String hypernyms = sc.nextLine();
+		String type = sc.nextLine();
+		File file2 = new File("Files\\" + hypernyms);
+		HashMap<Integer, Integer> hypernymshm = new HashMap<Integer, Integer>();
+		int vertices = synsetshm.size();
+		Digraph d = new Digraph(vertices);
+		try {
+			Scanner scan1 = new Scanner(file2);
+			while (scan1.hasNextLine()) {
+				String[] inp = scan1.nextLine().split(",");
+				d.addEdge(Integer.parseInt(inp[0]), Integer.parseInt(inp[1]));
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
+		System.out.println(d);
 	}
 }
