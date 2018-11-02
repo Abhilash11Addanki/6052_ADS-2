444d286ed4c63bfa880566c1be72678f31e30618
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-11-02 23:49:31 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 Assignments/Module 4/Code Camp - WordNet/WordNet/Stack.java
+++ ADS-2 Assignments/Module 4/Code Camp - WordNet/WordNet/Stack.java
@@ -134,7 +134,6 @@ public class Stack<Item> implements Iterable<Item> {
          * @return     Item.
          */
         public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
             Item item = current.item;
             current = current.next; 
             return item;
