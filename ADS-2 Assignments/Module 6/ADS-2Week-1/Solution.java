dc3def4cc757650d24f135193186358a0da967d6
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-11-03 11:14:07 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 Assignments/Module 6/ADS-2Week-1/Solution.java
+++ ADS-2 Assignments/Module 6/ADS-2Week-1/Solution.java
@@ -1,6 +1,27 @@
 import java.util.Scanner;
 class PageRank {
-	
+	private Digraph dg;
+	private double pagerank;
+	PageRank(final Digraph d) {
+		dg = d;
+		pagerank = 1 / 4;
+	}
+	double getPR(int v) {
+		pagerank = 0;
+		for (Integer i : dg.adj(v)) {
+			pagerank += getPR(i) / dg.outdegree(v);
+		}
+		return pagerank;
+	}
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+		for (int v = 0; v < dg.V(); v++) {
+			s.append(String.format("%d - ", v));
+			s.append(String.format("%d ", getPR(v)));
+			s.append("\n");
+		}
+		return s.toString();
+	}
 }
 
 class WebSearch {
@@ -25,21 +46,21 @@ public class Solution {
 		}
 		System.out.println(d);
 		// Create page rank object and pass the graph object to the constructor
-		
+		PageRank p = new PageRank(d);
 		// print the page rank object
-		
+		System.out.println(p);
 		// This part is only for the final test case
-		
+
 		// File path to the web content
 		String file = "WebContent.txt";
-		
+
 		// instantiate web search object
 		// and pass the page rank object and the file path to the constructor
-		
+
 		// read the search queries from std in
 		// remove the q= prefix and extract the search word
 		// pass the word to iAmFeelingLucky method of web search
 		// print the return value of iAmFeelingLucky
-		
+
 	}
 }
