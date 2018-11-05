29002a69070d6175c9a84d23f03973203c596ad8
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-11-05 09:00:12 +0530

    "ads2_m6_a1_1: 5/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 Assignments/Module 6/ADS-2Week-1/Solution.java
+++ ADS-2 Assignments/Module 6/ADS-2Week-1/Solution.java
@@ -4,77 +4,77 @@ import java.util.Arrays;
  * Class for page rank.
  */
 class PageRank {
-	/**
-	 * graph as g.
-	 */
-	private Digraph dg;
-	/**
-	 * variable for vertices.
-	 */
-	private int vertices;
-	/**
-	 * array to store the pageRanks.
-	 */
-	private Double[] pagerank;
-	/**
-	 * temp array to store the page ranks.
-	 */
-	private Double[] temppagerank;
-	/**
-	 * constructor.
-	 *
-	 * @param      gph    The graphics
-	 */
-	PageRank(final Digraph d) {
-		dg = d;
-		vertices = dg.vertices();
-		pagerank = new Double[vertices];
-		temppagerank = new Double[vertices];
-		for (int i = 0; i < vertices; i++) {
-			pagerank[i] = 1.0 / vertices;
-		}
-		for (int i = 0; i < vertices; i++) {
-			if (dg.outdegree(i) == 0) {
-				for (int j = 0; j < vertices; j++) {
-					if (i != j) {
-						dg.addEdge(i, j);
-					}
-				}
-			}
-		}
-		final int thousand = 1000;
-		for (int k = 1; k < thousand; k++) {
-			for (int i = 0; i < vertices; i++) {
-				temppagerank[i] = pagerank[i];
-				pagerank[i] = 0.0;
-			}
-			for (int j = 0; j < vertices; j++) {
-				for (int ele : dg.reverse().adj(j)) {
-					pagerank[j] += temppagerank[ele] / dg.outdegree(ele);
-				}
-			}
-		}
-	}
-	/**.
-	 * method to get the page rank for the given page rank
-	 *
-	 * @param      v     { vertices of type int }
-	 *
-	 * @return     The page rank.
-	 */
-	public Double getPageRank(final int v) {
-		return pagerank[v];
-	}
-	/**.
-	 * method to printer
-	 */
-	public String toString() {
-		String s = "";
-		for (int i = 0; i < vertices; i++) {
-			s += i + " - " + pagerank[i] + "\n";
-		}
-		return s;
-	}
+    /**
+     * graph as g.
+     */
+    private Digraph dg;
+    /**
+     * variable for vertices.
+     */
+    private int vertices;
+    /**
+     * array to store the pageRanks.
+     */
+    private Double[] pagerank;
+    /**
+     * temp array to store the page ranks.
+     */
+    private Double[] temppagerank;
+    /**
+     * constructor.
+     *
+     * @param      gph    The graphics
+     */
+    PageRank(final Digraph d) {
+        dg = d;
+        vertices = dg.vertices();
+        pagerank = new Double[vertices];
+        temppagerank = new Double[vertices];
+        for (int i = 0; i < vertices; i++) {
+            pagerank[i] = 1.0 / vertices;
+        }
+        for (int i = 0; i < vertices; i++) {
+            if (dg.outdegree(i) == 0) {
+                for (int j = 0; j < vertices; j++) {
+                    if (i != j) {
+                        dg.addEdge(i, j);
+                    }
+                }
+            }
+        }
+        final int thousand = 1000;
+        for (int k = 1; k < thousand; k++) {
+            for (int i = 0; i < vertices; i++) {
+                temppagerank[i] = pagerank[i];
+                pagerank[i] = 0.0;
+            }
+            for (int j = 0; j < vertices; j++) {
+                for (int ele : dg.reverse().adj(j)) {
+                    pagerank[j] += temppagerank[ele]
+                    / dg.outdegree(ele);
+                }
+            }
+        }
+    }
+    /**
+     * Gets the page rank.
+     * @param      v     vertex.
+     * @return     The page rank.
+     */
+    public Double getPageRank(final int v) {
+        return pagerank[v];
+    }
+    /**
+     * Returns a string representation of the object.
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < vertices; i++) {
+            s += i + " - " + pagerank[i] + "\n";
+        }
+        return s;
+    }
 }
 /**
  * Class for web search.
@@ -86,47 +86,47 @@ class WebSearch {
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-	}
-	/**
-	 * main method that drives the program.
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		// read the first line of the input to get the number of vertices
-		int n = Integer.parseInt(sc.nextLine());
-		Digraph d = new Digraph(n);
-		// iterate count of vertices times
-		// to read the adjacency list from std input
-		// and build the graph
-		for (int i = 0; i < n; i++) {
-			String[] inp = sc.nextLine().split(" ");
-			for (int j = 1; j < inp.length; j++) {
-				d.addEdge(Integer.parseInt(inp[0]),
-				          Integer.parseInt(inp[j]));
-			}
-		}
-		System.out.println(d);
-		// Create page rank object and pass the graph object to the constructor
-		PageRank p = new PageRank(d);
-		// print the page rank object
-		System.out.println(p);
-		// This part is only for the final test case
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**
+     * main method that drives the program.
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // read the first line of the input to get the number of vertices
+        int n = Integer.parseInt(sc.nextLine());
+        Digraph d = new Digraph(n);
+        // iterate count of vertices times
+        // to read the adjacency list from std input
+        // and build the graph
+        for (int i = 0; i < n; i++) {
+            String[] inp = sc.nextLine().split(" ");
+            for (int j = 1; j < inp.length; j++) {
+                d.addEdge(Integer.parseInt(inp[0]),
+                          Integer.parseInt(inp[j]));
+            }
+        }
+        System.out.println(d);
+        // Create page rank object and pass the graph object to the constructor
+        PageRank p = new PageRank(d);
+        // print the page rank object
+        System.out.println(p);
+        // This part is only for the final test case
 
-		// File path to the web content
-		String file = "WebContent.txt";
+        // File path to the web content
+        String file = "WebContent.txt";
 
-		// instantiate web search object
-		// and pass the page rank object and the file path to the constructor
+        // instantiate web search object
+        // and pass the page rank object and the file path to the constructor
 
-		// read the search queries from std in
-		// remove the q= prefix and extract the search word
-		// pass the word to iAmFeelingLucky method of web search
-		// print the return value of iAmFeelingLucky
+        // read the search queries from std in
+        // remove the q= prefix and extract the search word
+        // pass the word to iAmFeelingLucky method of web search
+        // print the return value of iAmFeelingLucky
 
-	}
+    }
 }
