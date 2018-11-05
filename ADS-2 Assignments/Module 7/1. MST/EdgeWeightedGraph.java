ab06c72bd44e29e92b556a0d4c4c83182b5ae8fc
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-11-05 19:30:35 +0530

    "ads2_m7_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 Assignments/Module 7/1. MST/EdgeWeightedGraph.java
+++ ADS-2 Assignments/Module 7/1. MST/EdgeWeightedGraph.java
@@ -1,113 +1,96 @@
-/**
- * Class for edge weighted graph.
- */
 public class EdgeWeightedGraph {
-    /**
-     * no of vertices.
-     */
-    private final int v;
-    /**
-     * no of edges.
-     */
-    private int e;
-    /**
-     * adj array of type Bag.
-     */
+    private static final String NEWLINE = System.getProperty("line.separator");
+
+    private final int V;
+    private int E;
     private Bag<Edge>[] adj;
+    
     /**
-     * Initializes an empty edge-weighted graph with
-     * {@code V} vertices and 0 edges.
-     * @param  v1 the number of vertices
+     * Initializes an empty edge-weighted graph with {@code V} vertices and 0 edges.
+     *
+     * @param  V the number of vertices
+     * @throws IllegalArgumentException if {@code V < 0}
      */
-    public EdgeWeightedGraph(final int v1) {
-        this.v = v1;
-        this.e = 0;
-        adj = (Bag<Edge>[]) new Bag[v1];
-        for (int i = 0; i < v1; i++) {
-            adj[i] = new Bag<Edge>();
-        }
-    }
-    /**
-     * Initializes a new edge-weighted graph that
-     * is a deep copy of {@code G}.
-     * @param  gp the edge-weighted graph to copy
-     */
-    public EdgeWeightedGraph(final EdgeWeightedGraph gp) {
-        this(gp.vertices());
-        this.e = gp.edges();
-        for (int v1 = 0; v1 < gp.vertices(); v1++) {
-            Stack<Edge> reverse = new Stack<Edge>();
-            for (Edge e1 : gp.adj[v1]) {
-                reverse.push(e1);
-            }
-            for (Edge e1 : reverse) {
-                adj[v1].add(e1);
-            }
+    public EdgeWeightedGraph(int V) {
+        if (V < 0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Edge>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<Edge>();
         }
     }
     /**
      * Returns the number of vertices in this edge-weighted graph.
+     *
      * @return the number of vertices in this edge-weighted graph
      */
-    public int vertices() {
-        return v;
+    public int V() {
+        return V;
     }
+
     /**
      * Returns the number of edges in this edge-weighted graph.
      *
      * @return the number of edges in this edge-weighted graph
      */
-    public int edges() {
-        return e;
+    public int E() {
+        return E;
     }
     /**
      * Adds the undirected edge {@code e} to this edge-weighted graph.
      *
-     * @param  e1 the edge
+     * @param  e the edge
+     * @throws IllegalArgumentException unless both endpoints are between {@code 0} and {@code V-1}
      */
-    public void addEdge(final Edge e1) {
-        int v1 = e1.either();
-        int w = e1.other(v1);
-        adj[v].add(e1);
-        adj[w].add(e1);
-        e++;
+    public void addEdge(Edge e) {
+        int v = e.either();
+        int w = e.other(v);
+        adj[v].add(e);
+        adj[w].add(e);
+        E++;
     }
+
     /**
      * Returns the edges incident on vertex {@code v}.
-     * @param  v1 the vertex
+     *
+     * @param  v the vertex
      * @return the edges incident on vertex {@code v} as an Iterable
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
      */
-    public Iterable<Edge> adj(final int v1) {
-        return adj[v1];
+    public Iterable<Edge> adj(int v) {
+        return adj[v];
     }
+
     /**
      * Returns the degree of vertex {@code v}.
-     * @param  v1 the vertex
-     * @return the degree of vertex {@code v}
+     *
+     * @param  v the vertex
+     * @return the degree of vertex {@code v}               
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
      */
-    public int degree(final int v1) {
-        return adj[v1].size();
+    public int degree(int v) {
+        return adj[v].size();
     }
+
     /**
      * Returns all edges in this edge-weighted graph.
-     * To iterate over the edges in this edge-weighted graph,
-     * use foreach notation:
+     * To iterate over the edges in this edge-weighted graph, use foreach notation:
      * {@code for (Edge e : G.edges())}.
      *
      * @return all edges in this edge-weighted graph, as an iterable
      */
-    public Iterable<Edge> noedges() {
+    public Iterable<Edge> edges() {
         Bag<Edge> list = new Bag<Edge>();
-        for (int i = 0; i < v; i++) {
+        for (int v = 0; v < V; v++) {
             int selfLoops = 0;
-            for (Edge e1 : adj(i)) {
-                if (e1.other(i) > i) {
-                    list.add(e1);
-                } else if (e1.other(i) == i) {
-                //(self loops will be consecutive)
-                    if (selfLoops % 2 == 0) {
-                        list.add(e1);
-                    }
+            for (Edge e : adj(v)) {
+                if (e.other(v) > v) {
+                    list.add(e);
+                }
+                // add only one copy of each self loop (self loops will be consecutive)
+                else if (e.other(v) == v) {
+                    if (selfLoops % 2 == 0) list.add(e);
                     selfLoops++;
                 }
             }
