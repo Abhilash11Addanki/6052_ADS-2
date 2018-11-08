bfe3f298f1efa14f8f62e22da73a025e76fc5477
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-11-08 17:11:52 +0530

    "ads2_m10_a1_1: 6/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 Assignments/Module 10/Seam Carving/SeamCarver.java
+++ ADS-2 Assignments/Module 10/Seam Carving/SeamCarver.java
@@ -109,15 +109,15 @@ public class SeamCarver {
 		for (int i = 0; i < w; i++)
 			pathTo[i] = -1;
 		for (int r = 1, i = w; r < h; r++) {
-			if (energy[i - w] <= energy[i - w + 1]) pathTo[i] = i - w - 1;
+			if (energy[i - w] <= energy[i - w + 1]) pathTo[i] = i - w;
 			else pathTo[i] = i - w + 1;
 			energy[i] += energy[pathTo[i]]; i++;
 			for (int c = 1; c < w - 1; c++, i++) {
 				if (energy[i - w - 1] <= energy[i - w]) {
-					if (energy[i - w - 1] <= energy[i - w + 1]) pathTo[i] = i - w - 1;
+					if (energy[i - w - 1] <= energy[i - w + 1]) pathTo[i] = i - w;
 					else pathTo[i] = i - w + 1;
 				} else {
-					if (energy[i - w] <= energy[i - w + 1]) pathTo[i] = i - w;
+					if (energy[i - w] <= energy[i - w + 1]) pathTo[i] = i - w - 1;
 					else pathTo[i] = i - w + 1;
 				}
 				energy[i] += energy[pathTo[i]];
