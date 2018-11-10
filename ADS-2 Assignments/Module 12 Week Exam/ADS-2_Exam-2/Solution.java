33c49c25e248ea94c32775f2e65a1dfe66c61dce
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-11-10 12:19:11 +0530

    "ads2_m12_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 Assignments/Module 12 Week Exam/ADS-2_Exam-2/Solution.java
+++ ADS-2 Assignments/Module 12 Week Exam/ADS-2_Exam-2/Solution.java
@@ -1,6 +1,3 @@
-/**
- * Dijkstra algorithm uses time complexity of O(E log(V)).
- */
 import java.util.Scanner;
 /**
  * Class for solution.
@@ -10,41 +7,35 @@ public final class Solution {
      * Constructs the object.
      */
     private Solution() {
-        /**
-         * unused constructor.
-         */
     }
     /**
-     * main function handles the input and returns the
-     * output to the console.
-     * for via path it takes N*N complexity to print.
-     * rest all depends on the Dijistra's algorithm
-     *
+     * This method drives the program.
      * @param      args  The arguments
+     * Time complexity for this method is O(E log V) Since
+     * it uses Dijkstra's Algorithm to find shortest path.
+     * where E is no of edges and V is no of vertices.
      */
     public static void main(final String[] args) {
         // Self loops are not allowed...
         // Parallel Edges are allowed...
         // Take the Graph input here...
-        Scanner scan = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int cities = scan.nextInt();
-        scan.nextLine();
-        int roadLines = scan.nextInt();
-        scan.nextLine();
-        EdgeWeightedGraph graph = new EdgeWeightedGraph(cities);
+        int vertices = Integer.parseInt(sc.nextLine());
+        int edges = Integer.parseInt(sc.nextLine());
+        // EdgeWeightedGraph to construct the graph.
+        EdgeWeightedGraph graph = new EdgeWeightedGraph(vertices);
+        for (int i = 0; i < edges; i++) {
+            String[] input = sc.nextLine().split(" ");
+            int verA = Integer.parseInt(input[0]);
+            int verB = Integer.parseInt(input[1]);
+            int distance = Integer.parseInt(input[2]);
 
-        for (int i = 0; i < roadLines; i++) {
-            String[] paths = scan.nextLine().split(" ");
-            int cityA = Integer.parseInt(paths[0]);
-            int cityB = Integer.parseInt(paths[1]);
-            int distance = Integer.parseInt(paths[2]);
-
-            graph.addEdge(new Edge(cityA, cityB, distance));
+            graph.addEdge(new Edge(verA, verB, distance));
 
         }
 
-        String caseToGo = scan.nextLine();
+        String caseToGo = sc.nextLine();
 
         switch (caseToGo) {
         case "Graph":
@@ -57,13 +48,12 @@ public final class Solution {
             // First is the source and second is the destination.
             // If the path exists print the distance between them.
             // Other wise print "No Path Found."
-            String[] str = scan.nextLine().split(" ");
-            int s = Integer.parseInt(str[0]);
-            int d = Integer.parseInt(str[1]);
-
-            DijkstraSP dj = new DijkstraSP(graph, s);
-
-            double dis = dj.distTo(d);
+            String[] str = sc.nextLine().split(" ");
+            int source = Integer.parseInt(str[0]);
+            int dest = Integer.parseInt(str[1]);
+            // Dijkstra's Algorithm.
+            DijkstraSP dsp = new DijkstraSP(graph, source);
+            double dis = dsp.distTo(dest);
             double inf = Double.POSITIVE_INFINITY;
             if (dis == inf) {
                 System.out.println("No Path Found.");
@@ -73,30 +63,28 @@ public final class Solution {
             break;
 
         case "ViaPaths":
-            String[] str1 = scan.nextLine().split(" ");
+            String[] str1 = sc.nextLine().split(" ");
             int src = Integer.parseInt(str1[0]);
             int via = Integer.parseInt(str1[1]);
             int des = Integer.parseInt(str1[2]);
 
-            DijkstraSP dj0 = new DijkstraSP(graph, src);
+            DijkstraSP dsp1 = new DijkstraSP(graph, src);
             // System.out.println(dj.pathTo(via));
-            if (dj0.hasPathTo(via)) {
-                DijkstraSP dj1 = new DijkstraSP(graph, via);
+            if (dsp1.hasPathTo(via)) {
+                DijkstraSP dsp2 = new DijkstraSP(graph, via);
 
-                if (dj1.hasPathTo(des)) {
-                    System.out.println(dj0.distTo(via) + dj1.distTo(des));
+                if (dsp2.hasPathTo(des)) {
+                    System.out.println(dsp1.distTo(via) + dsp2.distTo(des));
                     int temp = src;
                     System.out.print(temp);
                     int oth = 1;
-                    for (Edge i : dj0.pathTo(via)) {
+                    for (Edge i : dsp1.pathTo(via)) {
                         oth = temp;
                         System.out.print(" " + i.other(oth));
                         temp = i.other(oth);
                     }
-
                     while (oth != des) {
-                        for (Edge j : dj1.pathTo(des)) {
-
+                        for (Edge j : dsp2.pathTo(des)) {
                             try {
                                 oth = temp;
                                 System.out.print(" " + j.other(oth));
