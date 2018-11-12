674ccc5aaab3dba387231fff67b989e9669ed3ce
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-11-12 17:23:42 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 Assignments/Module 13/Radix Sorts/Quick3string.java
+++ ADS-2 Assignments/Module 13/Radix Sorts/Quick3string.java
@@ -113,7 +113,7 @@ public class Quick3string {
      * @param      a     String array.
      * @return     True if sorted, False otherwise.
      */
-    private boolean isSorted(String[] a) {
+    private boolean isSorted(final String[] a) {
         for (int i = 1; i < a.length; i++) {
             if (a[i].compareTo(a[i - 1]) < 0) {
                 return false;
