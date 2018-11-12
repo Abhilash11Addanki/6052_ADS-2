984e7882e73e7348cfc37df25d82e585831ff479
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-11-12 15:32:04 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 Assignments/Module 13/Radix Sorts/LSD.java
+++ ADS-2 Assignments/Module 13/Radix Sorts/LSD.java
@@ -7,7 +7,8 @@ public class LSD {
      */
     private static final int BITS_PER_BYTE = 8;
     /**
-      * Rearranges the array of W-character strings in ascending order.
+      * Rearranges the array of W-character
+      * strings in ascending order.
       *
       * @param a the array to be sorted
       * @param w the number of characters per string
@@ -50,10 +51,10 @@ public class LSD {
       * @param a the array to be sorted
       */
     public static void sort(final int[] a) {
-        final int BITS = 32;                 // each int is 32 bits
-        final int R = 1 << BITS_PER_BYTE;    // each bytes is between 0 and 255
-        final int MASK = R - 1;              // 0xFF
-        final int w = BITS / BITS_PER_BYTE;  // each int is 4 bytes
+        final int BITS = 32;
+        final int R = 1 << BITS_PER_BYTE;
+        final int MASK = R - 1;
+        final int w = BITS / BITS_PER_BYTE;
 
         int n = a.length;
         int[] aux = new int[n];
@@ -63,7 +64,8 @@ public class LSD {
             // compute frequency counts
             int[] count = new int[R + 1];
             for (int i = 0; i < n; i++) {
-                int c = (a[i] >> BITS_PER_BYTE * d) & MASK;
+                int c = (a[i] >> BITS_PER_BYTE * d)
+                & MASK;
                 count[c + 1]++;
             }
 
@@ -72,7 +74,6 @@ public class LSD {
                 count[r + 1] += count[r];
             }
 
-            // for most significant byte, 0x80-0xFF comes before 0x00-0x7F
             if (d == w - 1) {
                 int shift1 = count[R] - count[R / 2];
                 int shift2 = count[R / 2];
