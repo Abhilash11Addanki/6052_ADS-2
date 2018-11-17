bb6eb2b206158294b8e655d5b66c3e8bdc46dde1
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-11-17 13:01:53 +0530

    "ads2_m18_a1_1: 4/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 Assignments/Module 18 Week Exam/ADS-2Exam-3/Solution.java
+++ ADS-2 Assignments/Module 18 Week Exam/ADS-2Exam-3/Solution.java
@@ -125,20 +125,19 @@ class T9 {
 
 	public Iterable<String> potentialWords(String t9Signature) {
 		// your code goes here
-		HashMap<String, String> map = new HashMap<String, String>();
-	 	map.put("2", "abc");
-        map.put("3", "def");
-        map.put("4", "ghi");
-        map.put("5", "jkl");
-        map.put("6", "mno");
-        map.put("7", "pqrs");
-        map.put("8", "tuv");
-        map.put("9", "wxyz");
+		HashMap<Character, String> map = new HashMap<Character, String>();
+	 	map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
 		ArrayList<String> res = new ArrayList<String>();
         ArrayList<String> preres = new ArrayList<String>();
         ArrayList<String> result = new ArrayList<String>();
         res.add("");
-        preres.add("");
         if(t9Signature.length() == 1) {
         	return null;
         }
